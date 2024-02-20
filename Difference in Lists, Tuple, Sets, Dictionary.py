a58ef7d7da6924ad87b254129c9423fc9ed28df5
Manas Juneja <manasjunejaa@gmail.com>
2024-02-20 20:29:35 +0530

    Different Data Types

---
+++ Difference in Lists, Tuple, Sets, Dictionary.py
@@ -0,0 +1,19 @@
+#Lists
+#Mutuable data types means it can be edited
+list = ["apple", "ball", "cat"]
+print(list)
+
+#Tuple
+#Immutable data types cannot be edited
+Tuple = ("apple", "ball", "cat")
+print(Tuple)
+
+#Sets
+#Unordered data types can't be accessed with index
+Set = {95, 92, 97, 97, 955, 92}
+print(Set)
+
+#Dictionary
+#Used to print key-value pairs
+Dictionary = {"Chemistry" : 95, "Maths" : 99, "Physics" : 100}
+print(Dictionary["Chemistry"])
